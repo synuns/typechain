473affff6ab61aa6cb348ec6a67c2319d2682b0e
Jdentity <identityjang@gmail.com>
2021-04-29 00:14:38 +0900

    first typescript

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("hello");
+"use strict";
+exports.__esModule = true;
+var name = "identity", age = 24, gender = "male";
+var sayHi = function (name, age, gender) {
+    console.log("Hello " + name + ", you are " + age + ", you are a " + gender);
+};
+sayHi(name, age);
