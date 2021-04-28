64bccd318be2174e03e1aa32bd3a8678010cb7c0
Jdentity <identityjang@gmail.com>
2021-04-29 01:00:25 +0900

    interface in Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
-  const sayHi = (name:string , age:number, gender:string): string => {
-    return `Hello ${name}, you are ${age}, you are a ${gender}`;
+interface Human {
+  name:string;
+  age:number;
+  gender: string;
+}
+
+const person = {
+  name: "identity",
+  age: 24,
+  gender: "male"
+};
+
+
+  const sayHi = (person: Human): string => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
   };
 
-  sayHi("identity", 24, "male");
+  console.log(sayHi(person));
 
   export {};
