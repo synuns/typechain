6bc94be4f0320bb0a45b586eaedd98106e9ac56c
Jdentity <identityjang@gmail.com>
2021-04-29 01:06:11 +0900

    classes on Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
-interface Human {
-  name:string;
-  age:number;
-  gender: string;
+class Human {
+  public name:string;
+  public age:number;
+  public gender: string;
+  constructor(name: string, age:number, gender:string){
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "identity",
-  age: 24,
-  gender: "male"
-};
-
+const Me = new Human("Identity", 24, "male");
 
   const sayHi = (person: Human): string => {
     return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
   };
 
-  console.log(sayHi(person));
+  console.log(sayHi(Me));
 
   export {};
